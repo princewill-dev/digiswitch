10f113d3730da9276afd6e1eaa8fc84b990efc23
princewill-dev <eekudep@gmail.com>
2024-02-13 07:01:32 -0500

    remastered

---
+++ footer.php
@@ -0,0 +1,103 @@
+<footer id="footer-3" class="pt-100 footer">
+	<div class="container">
+
+
+		<!-- FOOTER CONTENT -->
+		<div class="row">
+
+
+			<!-- FOOTER LOGO -->
+			<div class="col-md-3">
+				<div class="footer-info">
+					<img class="footer-logo" src="images/logo-purple-white.png" alt="footer-logo">
+				</div>	
+			</div>	
+
+
+			<!-- FOOTER LINKS -->
+			<div class="col-md-3">
+				<div class="footer-links fl-1">
+				
+					<!-- Title -->
+					<h6 class="s-17 w-700">Company</h6>
+
+					<!-- Links -->
+					<ul class="foo-links clearfix">
+						<li><p><a href="#about-us">About Us</a></p></li>
+						<li><p><a href="#features">Our Services</a></p></li>
+					</ul>
+
+				</div>
+			</div>	<!-- END FOOTER LINKS -->	
+
+
+			<!-- FOOTER LINKS -->
+			<div class="col-md-3">
+				<div class="footer-links fl-2">
+									
+					<!-- Title -->
+					<h6 class="s-17 w-700">Product</h6>
+
+					<!-- Links -->
+					<ul class="foo-links clearfix">
+						<li><p><a href="#projects">Our Works</a></p></li>		
+					</ul>
+
+				</div>	
+			</div>	<!-- END FOOTER LINKS -->
+
+
+			<!-- FOOTER LINKS -->
+			<div class="col-md-3">
+				<div class="footer-links fl-4">
+									
+					<!-- Title -->
+					<h6 class="s-17 w-700">Connect With Us</h6>
+
+					<!-- Mail Link -->
+					<p class="footer-mail-link ico-25">
+						<a href="mailto:support@digiswitch.tech">support@digiswitch.tech</a>
+					</p>
+
+					
+
+				</div>	
+			</div>	<!-- END FOOTER LINKS -->	
+
+
+		</div>	<!-- END FOOTER CONTENT -->
+
+
+		<hr>	<!-- FOOTER DIVIDER LINE -->
+
+
+		<!-- BOTTOM FOOTER -->
+		<div class="bottom-footer">
+			<div class="row row-cols-1 row-cols-md-2 d-flex align-items-center">
+
+
+				<!-- FOOTER COPYRIGHT -->
+				<div class="col">
+					<div class="footer-copyright"><p class="p-sm">&copy; 2024 DigiSwitch Technologies. <span>All Rights Reserved</span></p></div>
+				</div>
+
+
+				<!-- FOOTER SECONDARY LINK -->
+				<div class="col">
+					<div class="bottom-secondary-link ico-15 text-end">
+						<!-- Social Links -->	
+    					<ul class="footer-socials ico-25 text-center clearfix">		
+    						<li><a href="#"><span class="flaticon-facebook"></span></a></li>
+    						<li><a href="#"><span class="flaticon-twitter"></span></a></li>
+    					</ul>
+					</div>
+				</div>
+
+
+			</div>  <!-- End row -->
+		</div>	<!-- END BOTTOM FOOTER -->
+
+
+	</div>
+	<!-- End container -->	
+</footer>
---
+++ nav-bar.php
@@ -0,0 +1,59 @@
+<header id="header" class="tra-menu navbar-dark light-hero-header white-scroll">
+	<div class="header-wrapper">
+
+
+		<!-- MOBILE HEADER -->
+	    <div class="wsmobileheader clearfix">	  	
+	    	<span class="smllogo"><img src="images/logo-purple-white.png" alt="mobile-logo"></span>
+	    	<a id="wsnavtoggle" class="wsanimated-arrow"><span></span></a>	
+	 	</div>
+
+
+	 	<!-- NAVIGATION MENU -->
+	  	<div class="wsmainfull menu clearfix">
+			<div class="wsmainwp clearfix">
+
+
+				<!-- HEADER BLACK LOGO -->
+				<div class="desktoplogo">
+					<a href="index.php" class="logo-black"><img src="images/logo-purple.png" alt="logo"></a>
+				</div>
+
+
+				<!-- HEADER WHITE LOGO -->
+				<div class="desktoplogo">
+					<a href="index.php" class="logo-white"><img src="images/logo-purple-white.png" alt="logo"></a>
+				</div>
+
+
+				<!-- MAIN MENU -->
+  				<nav class="wsmenu clearfix">
+					<ul class="wsmenu-list nav-theme">
+
+
+					    <!-- SIMPLE NAVIGATION LINK -->
+					    <li class="nl-simple" aria-haspopup="true"><a href="#about-us" class="h-link">About</a></li>
+					    
+				    	<li class="nl-simple" aria-haspopup="true"><a href="./#features" class="h-link">Our Services</a></li>
+
+				    	<li class="nl-simple" aria-haspopup="true"><a href="./#projects" class="h-link">Our Works</a></li>
+				    	
+				    	<!--<li class="nl-simple" aria-haspopup="true"><a href="./#why-us" class="h-link">Why Choose us?</a></li>-->
+
+
+					    <!-- SIGN UP BUTTON -->
+					    <li class="nl-simple" aria-haspopup="true">
+					    	<a href="contact-us.php" class="btn r-04 btn--theme hover--theme last-link">Free Consultation</a>
+					    </li> 
+
+
+					</ul>
+				</nav>	<!-- END MAIN MENU -->
+
+
+			</div>
+		</div>	<!-- END NAVIGATION MENU -->
+
+
+	</div>     <!-- End header-wrapper -->
+</header>
